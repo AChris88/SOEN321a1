4a46a62318f5e927f8e7f3051418792190ebb1e5
Chris <allardchris88@gmail.com>
2015-10-15 18:49:52 -0400

    Starting part WebSecStats.

--- src/cipher/AES256.java
+++ src/cipher/AES256.java
@@ -1,6 +1,8 @@
 package cipher;
 
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 import javax.xml.bind.DatatypeConverter;
 
@@ -13,16 +15,10 @@ public class AES256 {
 	private static String authenticatedCookie = "user=admin,tmstmp=1443657660";
 	// The difference observed is that the string length is clearly 4 characters
 	// shorter.
-	// However, as a result, this may (will) change the length of the generated
+	// However, as a result, this may change the length of the generated
 	// cipher.
-	// That being said, given the length of the cipher, we could potentially
-	// infer (unless the user's name has as many characters as the word
-	// 'anonymous') if the cookie belongs to an anonymous user or not given that
-	// the cipher length for anonymous users is known to be 44 characters long.
-	// This will remain true until the timestamp value has an extra digit added
-	// to it.
 
-	private static String plaintextAdminCookie = "user=admin,tmstmp=14436576609999";
+	private static String plaintextAnonCookie = "user=anonymous,tmstmp=1443657660";
 	private static String base64Cipher = "40mO35Yj9cAMFaaOcshT10VwVw6WmbvAEyrI6TxElFY=";
 
 	public static void main(String[] args) {
@@ -30,16 +26,27 @@ public class AES256 {
 	}
 
 	private static void base64Decrypt(String cipher) {
-		byte[] tmp1 = cipher.getBytes();
-		byte[] tmp2 = DatatypeConverter.parseBase64Binary(cipher);
+		/*
+		 		byte[] message = "hello world".getBytes(StandardCharsets.UTF_8);
+				String encoded = Base64.getEncoder().encodeToString(message);
+				byte[] decoded = Base64.getDecoder().decode(encoded);
+		
+				System.out.println(encoded);
+				System.out.println(new String(decoded, StandardCharsets.UTF_8));
+				
+		 */
+		
+		byte[] tmp1 = cipher.getBytes(StandardCharsets.UTF_8);
+		byte[] tmp2 = plaintextAnonCookie.getBytes(StandardCharsets.UTF_8);
 
-		System.out.println(cipher);
-
+		System.out.println(new String(tmp1));
+		
+		System.out.println("tmp1: " + tmp1.length + " tmp2: " + tmp2.length);
 		for (int i = 0; i < tmp2.length; i++) {
 			tmp1[i] = (byte) (tmp1[i] ^ tmp2[i]);
 		}
 
-		String output = new String(tmp1);
+		String output = new String(tmp1, StandardCharsets.UTF_8);
 
 		System.out.println(output);
 	}
--- src/stats/WebSecStats.java
+++ src/stats/WebSecStats.java
@@ -1,14 +1,21 @@
 package stats;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WebSecStats {
 
 	public static void main(String[] args) {
-		getStartIndex(27026188);
-		getStartIndex(27077076);
+		int[] ranges = {getStartIndex(27026188), getStartIndex(27077076)};
 	}
 
 	private static int getStartIndex(int studentId) {
@@ -22,7 +29,42 @@ public class WebSecStats {
 
 		md.update(new Integer(studentId).toString().getBytes());
 		BigInteger bi = new BigInteger(1, md.digest());
-		return bi.mod(new BigInteger("9890"))
-				.multiply(new BigInteger("100")).intValue() + 1000;
+		return bi.mod(new BigInteger("9890")).multiply(new BigInteger("100"))
+				.intValue() + 1000;
+	}
+
+	private void analyzeSites(String path, int[] ranges) {
+		///
+		path = "Oct_13_2015_top-1m.csv";
+		///
+		BufferedReader br = null;
+		FileReader fr = null;
+		List<String> lines = new ArrayList<String>();
+		String line = null;
+		try {
+			fr = new FileReader(path);
+			br = new BufferedReader(fr);
+			while ((line = br.readLine()) != null) {
+				lines.add(line);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				br.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	private void recordAnalysis(String analysis) {
+		try {
+			Files.write(Paths.get("analysis.csv"), analysis.getBytes(),
+					StandardOpenOption.APPEND);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
