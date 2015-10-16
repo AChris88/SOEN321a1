e36768c86dce753ea07d6268793b6fbabc9af68e
Chris <allardchris88@gmail.com>
2015-10-16 00:19:32 -0400

    Analysis test run.

--- src/stats/WebSecStats.java
+++ src/stats/WebSecStats.java
@@ -4,13 +4,16 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.math.BigInteger;
+import java.net.ConnectException;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.Certificate;
+
 import javax.net.ssl.SSLSession;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
@@ -20,79 +23,8 @@ public class WebSecStats {
 	public static void main(String[] args) {
 		int[] ranges = { getStartIndex(27026188), getStartIndex(27077076) };
 		String path = "Oct_13_2015_top-1m.csv";
-		// readSiteListing(path, ranges);
-		analyzeSite(1, "google.com");
-	}
-
-	private static void analyzeSite(int rank, String domain) {
-		// System.setProperty("javax.net.ssl.trustStore", "clienttrust");
-		System.setProperty("https.protocols", "SSLv3,TLSv1,TLSv1.1,TLSv1.2");
-
-		SSLSocketFactory factory = null;
-		Socket socket = null;
-		SSLSession session = null;
-		Certificate[] certificates = null;
-
-		String protocol = "";
-		String cipherSuite = "";
-		String host = "";
-		String keyType = "";
-		String keySize = "";
-		String algorithm = "";
-		
-		boolean isHTTPS = false;
-		boolean isHSTS = false;
-		boolean isHSTSLong = false;
-		
-		String analysis = "";
-		try {
-			factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
-			socket = factory.createSocket(domain, 443);
-			session = ((SSLSocket) socket).getSession();
-			certificates = session.getPeerCertificates();
-
-//			System.out.println("Certificates:");
-//			for (int i = 0; i < certificates.length; i++) {
-//				System.out.println(((X509Certificate) certificates[i])
-//						.getSubjectDN());
-//			}
-//
-//			System.out.println("Peer host is " + session.getPeerHost());
-//			System.out.println("Cipher is " + session.getCipherSuite());
-//			System.out.println("Protocol is " + session.getProtocol());
-//			System.out.println("Session: " + session);
-			
-			host = session.getPeerHost();
-			protocol = session.getProtocol();
-			cipherSuite = session.getCipherSuite();
-			
-			algorithm = cipherSuite.substring(cipherSuite.lastIndexOf('_') + 1);
-			
-			if (protocol.equals("SSLv3") || protocol.equals("TLSv1")
-					|| protocol.equals("TLSv1.1") || protocol.equals("TLSv1.2"))
-				isHTTPS = true;
-
-			if(cipherSuite.contains("ECDHE"))
-				keyType = "ECDHE";
-			else if(cipherSuite.contains("ECDSA"))
-				keyType = "ECDSA";
-			else if (cipherSuite.contains("_RSA_"))
-				keyType = "RSA";
-			
-			keySize = cipherSuite.substring(cipherSuite.indexOf("AES_") + 4, cipherSuite.indexOf("_GCM_"));
-			
-			analysis = rank + "," + host + "," + isHTTPS + "," + protocol + "," + keyType + "," + keySize + "," + algorithm + "," + isHSTS + "," + isHSTSLong;
-			
-			System.out.println(analysis);
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				socket.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
+		readSiteListing(path, ranges);
+		// analyzeSite(1, "google.com");
 	}
 
 	private static int getStartIndex(int studentId) {
@@ -110,6 +42,122 @@ public class WebSecStats {
 				.intValue() + 1000;
 	}
 
+	private static void analyzeSite(int rank, String domain) {
+		// System.setProperty("javax.net.ssl.trustStore", "clienttrust");
+		System.setProperty("https.protocols", "SSLv3,TLSv1,TLSv1.1,TLSv1.2");
+
+		SSLSocketFactory factory = null;
+		Socket socket = null;
+		SSLSession session = null;
+		Certificate[] certificates = null;
+
+		String protocol = "";
+		String cipherSuite = "";
+		String host = domain;
+		String keyType = "";
+		String keySize = "";
+		String algorithm = "";
+
+		boolean isHTTPS = true;
+		boolean isHSTS = true;
+		boolean isHSTSLong = true;
+		boolean hostFound = true;
+		boolean connectionError = false;
+		
+		String analysis = "";
+		try {
+			factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+			socket = factory.createSocket(domain, 443);
+			session = ((SSLSocket) socket).getSession();
+
+			// certificates = session.getPeerCertificates();
+
+			// System.out.println("Certificates:");
+			// for (int i = 0; i < certificates.length; i++) {
+			// System.out.println(((X509Certificate) certificates[i])
+			// .getSubjectDN());
+			// }
+			//
+			// System.out.println("Peer host is " + session.getPeerHost());
+			// System.out.println("Cipher is " + session.getCipherSuite());
+			// System.out.println("Protocol is " + session.getProtocol());
+			// System.out.println("Session: " + session);
+
+			if (session != null) {
+				host = session.getPeerHost();
+				protocol = session.getProtocol();
+				cipherSuite = session.getCipherSuite();
+
+				algorithm = cipherSuite
+						.substring(cipherSuite.lastIndexOf('_') + 1);
+
+				if (protocol.equals("SSLv3") || protocol.equals("TLSv1")
+						|| protocol.equals("TLSv1.1")
+						|| protocol.equals("TLSv1.2"))
+					isHTTPS = true;
+
+				if (cipherSuite.contains("ECDHE"))
+					keyType = "ECDHE";
+				else if (cipherSuite.contains("ECDSA"))
+					keyType = "ECDSA";
+				else if (cipherSuite.contains("_RSA_"))
+					keyType = "RSA";
+
+				if (cipherSuite.contains("AES_")
+						&& cipherSuite.contains("_GCM_"))
+					keySize = cipherSuite.substring(
+							cipherSuite.indexOf("AES_") + 4,
+							cipherSuite.indexOf("_GCM_"));
+			}
+		} catch (UnknownHostException e) {
+			// 404 response code
+			hostFound = false;
+		} catch (ConnectException e) {
+			if (e.getMessage().contains("Connection refused")) {
+				// cannot connect due to set protocol property as only accepting
+				// SSL/TLS
+				isHSTS = false;
+			} else {
+				connectionError = true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			String errorMessage = "";
+			if (connectionError){
+				errorMessage = "connection error";
+				analysis = rank + "," + host + "," + errorMessage + ","
+						+ errorMessage + "," + errorMessage + ","
+						+ errorMessage + "," + errorMessage + ","
+						+ errorMessage + "," + errorMessage
+						+ "\n";
+			} else if (!hostFound) {
+				errorMessage = "404 response code";
+				analysis = rank + "," + host + "," + errorMessage + ","
+						+ errorMessage + "," + errorMessage + ","
+						+ errorMessage + "," + errorMessage + ","
+						+ errorMessage + "," + errorMessage
+						+ "\n";
+			} else if (!isHSTS) {
+				isHSTSLong = false;
+				isHTTPS = false;
+				analysis = rank + "," + host + "," + isHTTPS + ",,,,," + isHSTS
+						+ "," + isHSTSLong + "\n";
+			} else {
+				analysis = rank + "," + host + "," + isHTTPS + "," + protocol
+						+ "," + keyType + "," + keySize + "," + algorithm + ","
+						+ isHSTS + "," + isHSTSLong + "\n";
+			}
+			recordAnalysis(analysis);
+			try {
+				if (socket != null)
+					socket.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 	private static void readSiteListing(String path, int[] ranges) {
 		int startRange1 = ranges[0];
 		int endRange1 = startRange1 + 10000;
@@ -121,7 +169,7 @@ public class WebSecStats {
 		FileReader fr = null;
 		String line = null;
 
-		int recordNum = 1;
+		int recordNum = 0;
 		String url = "";
 
 		try {
@@ -131,8 +179,7 @@ public class WebSecStats {
 			while ((line = br.readLine()) != null) {
 				recordNum = Integer.parseInt(line.substring(0,
 						line.indexOf(',')));
-				url = line.substring(line.indexOf(','),
-						line.indexOf(line.indexOf(','), line.indexOf(',') + 1));
+				url = line.substring(line.indexOf(',') + 1);
 				if (recordNum <= 1000
 						|| (recordNum >= startRange1 && recordNum < endRange1)
 						|| (recordNum >= startRange2 && recordNum < endRange2)) {
@@ -152,7 +199,7 @@ public class WebSecStats {
 		}
 	}
 
-	private void recordAnalysis(String analysis) {
+	private static void recordAnalysis(String analysis) {
 		try {
 			Files.write(Paths.get("analysis.csv"), analysis.getBytes(),
 					StandardOpenOption.APPEND);
